828f099d0d05cb1a31faea4bbb90b35b2d57b817
Gabriela Cahuich Gomez <Gabriela Cahuich Gomez>
2026-04-17 08:19:44 -0600

    Primera version del proyecto de Proyectores

---
+++ README.md
@@ -0,0 +1,2 @@
+** Proyectos **
+Proyectos es una aplicacion web de ASP.NET que gestionara las reservaciones y prestamos de pro2yectores a los docentes. 
---
+++ WebApp/Controllers/proyectoresController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class Proyectores : Controller
+    {
+        public string Index()
+        {
+            return "Aquí se mostrará la lista de proyectores registrados";
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            string html = $"<h1>Nuevo proyector</h1>Aqu&iacute; se mostrar&aacute; la informaci&oacute; detallada del proyector con el ID: <strong>{id}</strong>";
+
+            return Content(html, "text/html");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            string html = $"<h1>Dar De Baja</h1>Aquí se darmostrar&aacute; de baja el proyector <strong>#{id}</strong>";
+            return Content(html, "text/html");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            string html = $"Aquí se modificará la información de un proyector específico con el ID: <strong>{id}</strong>";
+            return Content(html, "text/html");
+        }
+    }
+}
+   
